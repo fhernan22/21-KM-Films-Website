2285e78ed0734fe435bfb8fb8064c92f0855304d
fhernan22 <fhern103@fiu.edu>
2020-08-27 23:53:38 -0400

    Add dark and light themes

---
+++ src/components/Header.jsx
@@ -0,0 +1,52 @@
+import React, { useEffect } from "react"
+import { HeaderNav, Logo, Menu } from "../styles/HeaderStyles"
+import { Container, Flex } from "../styles/GlobalStyles"
+import { Link } from "gatsby"
+
+import {
+  useGlobalStateContext,
+  useGlobalDispatchContext,
+} from "../context/globalContext"
+
+const Header = () => {
+  const { currentTheme } = useGlobalStateContext()
+  const dispatch = useGlobalDispatchContext()
+
+  const toggleTheme = () => {
+    if (currentTheme === "dark") {
+      dispatch({ type: "TOGGLE_THEME", theme: "light" })
+    } else {
+      dispatch({ type: "TOGGLE_THEME", theme: "dark" })
+    }
+  }
+
+  useEffect(() => {
+    window.localStorage.setItem("theme", currentTheme)
+  }, [currentTheme])
+
+  return (
+    <HeaderNav
+      animate={{ y: 0, opacity: 1 }}
+      initial={{ y: -72, opacity: 0 }}
+      transition={{ duration: 1, ease: [0.6, 0.05, -0.01, 0.9] }}
+    >
+      <Container>
+        <Flex spaceBetween noHeight>
+          <Logo>
+            <Link to="/">HENRR</Link>
+            <span onClick={toggleTheme}></span>
+            <Link to="/">Y</Link>
+          </Logo>
+          <Menu>
+            <button>
+              <span></span>
+              <span></span>
+            </button>
+          </Menu>
+        </Flex>
+      </Container>
+    </HeaderNav>
+  )
+}
+
+export default Header
